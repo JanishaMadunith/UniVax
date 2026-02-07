8ba15bfaf1871a5e74cff8b7c6c322f40136901b
JanishaMadunith <janishamadunith@gmail.com>
2026-02-07 16:39:55 +0530

    fix

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="bg-gray-800 p-10 rounded-2xl shadow-xl text-center">
+        <h1 className="text-4xl font-bold text-cyan-400 mb-6">
+          React + Vite + Tailwind
+        </h1>
+
+        <button className="px-6 py-3 bg-cyan-500 hover:bg-cyan-600 text-white rounded-lg transition">
+          Tailwind Working 🚀
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
