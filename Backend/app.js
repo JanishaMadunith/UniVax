9a815e56d4d30abb620de9fdf90b6cf5b76635b2
tharusha-gunaweera <tharushagunaweera@gmail.com>
2026-02-08 13:48:52 +0530

    entrypoint created

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,13 @@
-//ra4mOFFVNUAgBbSs
 const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
 
-app.use("/",(req, res, next)=>{
-    res.send("It Is working")
-})
+// Middleware
+app.use(express.json());
 
-mongoose.connect("mongodb+srv://tharusha:ra4mOFFVNUAgBbSs@cluster0.xeknc.mongodb.net/")
-.then(()=>console.log("Connected to MogoDB"))
-.then(() => {
-    app.listen(5001);
-})
-.catch((err)=>console.log((err)));
+// Routes
+app.use("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
+module.exports = app;
