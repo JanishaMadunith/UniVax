1e84200027749ac503f398dac680e45809de302c
Wickramasinghe Arachchige Sajith Tharaka <129993532+Sajith22@users.noreply.github.com>
2026-02-14 12:11:39 +0530

    fix:003 up-to-date-to feb 14th 2026

--- Backend/app.js
+++ Backend/app.js
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-// const express = require("express");
-=======
 const express = require("express");
 const userRoutes = require("./Routes/Tharusha/UserRoutes");
->>>>>>> 2d9fddee0c06725c4e47070a13f404e9b0b32269
 
-// const app = express();
+const app = express();
 
-// // Middleware
-// app.use(express.json());
+// Middleware
+app.use(express.json());
 
-<<<<<<< HEAD
-// // Routes
-// app.use("/", (req, res) => {
-//   res.send("Backend is working!");
-// });
-=======
 // Routes
 app.use("/users", userRoutes);
->>>>>>> 2d9fddee0c06725c4e47070a13f404e9b0b32269
 
-// module.exports = app;
+module.exports = app;
