4bc7a6deebe702353489b83e7268d886a6e1970e
J.D.Jayatilake <84485753+dillonjayatilake@users.noreply.github.com>
2026-02-21 14:47:45 +0530

    Vaccine Catalogue file added

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ if (!MONGO_URI) {
 
 // Connect to MongoDB and start server
 mongoose
-  .connect(MONGO_URI) //Database
+  .connect(MONGO_URI) 
   .then(() => {
     console.log("✓ Connected to MongoDB");
     app.listen(PORT, () => {
