492c2525b36981e8ba670203f9319f8adc29dcdd
Tharusha Gunaweera <your_email@gmail.com>
2026-02-22 09:42:24 +0530

    combined app.js to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,21 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const app = require("./app");
+const express = require("express");
+const userRoutes = require("./Routes/Tharusha/UserRoutes");
+const vaccineRoutes = require("./Routes/Dillon/VaccineRoutes");
+const doseRoutes = require("./Routes/Dillon/DoseRoutes");
+const appointmentRoutes = require("./Routes/Janeesha/AppointmentRoutes");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/users", userRoutes);
+app.use("/vaccines", vaccineRoutes);
+app.use("/doses", doseRoutes);
+app.use("/appointments", appointmentRoutes);
 
 const PORT = process.env.PORT || 5001;
 const MONGO_URI = process.env.MONGO_URI;
