defab92a2fa0b9a53caff930988f0a99ffffc2e4
Tharusha Gunaweera <159910129+tharusha-gunaweera@users.noreply.github.com>
2026-02-27 14:59:10 +0530

    Changed pauser to patient

--- Backend/Routes/ImmunizationLogs/immunizationLog.routes.js
+++ Backend/Routes/ImmunizationLogs/immunizationLog.routes.js
@@ -4,10 +4,10 @@ const { createLog, getLogs, getLogById, updateLog, deleteLog } = require("../../
 const authMiddleware = require('../../middlewares/auth.middleware.js');  // Placeholder for JWT/role check
 
 // Protected routes
-router.post('/', authMiddleware(['doctor', 'admin']), createLog);  // Only doctors/admins create
-router.get('/', authMiddleware(['user', 'doctor', 'admin']), getLogs);
-router.get('/:id', authMiddleware(['user', 'doctor', 'admin']), getLogById);
-router.put('/:id', authMiddleware(['doctor', 'admin']), updateLog);
-router.delete('/:id', authMiddleware(['admin']), deleteLog);  // Only admins delete
+router.post('/', authMiddleware(['Doctor', 'Admin']), createLog);  // Only doctors/admins create
+router.get('/', authMiddleware(['Patient', 'Doctor', 'Admin']), getLogs);
+router.get('/:id', authMiddleware(['Patient', 'Doctor', 'Admin']), getLogById);
+router.put('/:id', authMiddleware(['Doctor', 'Admin']), updateLog);
+router.delete('/:id', authMiddleware(['Admin']), deleteLog);  // Only admins delete
 
 module.exports = router;
