e39f243e1bde22666de4669b04514e92343db5db
Janisha Samarathunga <160045041+JanishaMadunith@users.noreply.github.com>
2026-02-27 18:15:46 +0530

    MVC and JWT for clinics created

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const userRoutes = require("./Routes/users/UserRoutes");
 const vaccineRoutes = require("./Routes/vaccineCatalog/VaccineRoutes");
 const doseRoutes = require("./Routes/vaccineCatalog/DoseRoutes");
 const appointmentRoutes = require("./Routes/Appointment/AppointmentRoutes");
+const clinicRoutes = require("./Routes/Appointment/ClinicRoutes");
 const authMiddleware = require("./middlewares/auth.middleware");  // Placeholder for JWT/role check
 const immunizationLogRoutes = require("./Routes/ImmunizationLogs/immunizationLog.routes");  // New route for immunization logs
 
@@ -18,6 +19,7 @@ app.use("/api/V1/users", userRoutes);
 app.use("/api/V1/vaccines", vaccineRoutes);
 app.use("/api/V1/doses", doseRoutes);
 app.use("/api/V1/appointments", appointmentRoutes);
+app.use("/api/V1/clinics", clinicRoutes);
 app.use("/api/V1/logs",immunizationLogRoutes)  // Register immunization log routes
 
 const PORT = process.env.PORT || 5001;
