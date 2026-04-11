75286ca441c8e394f1906a2c44ae72b324f6c4e6
Tharusha Gunaweera <159910129+tharusha-gunaweera@users.noreply.github.com>
2026-04-11 21:23:35 +0530

    User test file wrong path edited

--- Backend/Routes/users/UserRoutes.js
+++ Backend/Routes/users/UserRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const UserController = require("../../Controllers/Users/UserControllers");
+const UserController = require("../../Controllers/users/UserController");
 const authMiddleware = require("../../middlewares/auth.middleware");
 const upload = require('../../middlewares/upload.middleware');
 
